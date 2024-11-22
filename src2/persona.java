bd61ef7460cbf101c432b6f6dbc6ff8b00a5e4f1
RENE FUENTES GUZMAN <renefuengu@gmail.com>
2024-11-22 12:00:07 -0600

    Update persona.java

--- src2/persona.java
+++ src2/persona.java
@@ -1,1 +1,32 @@
 
+public class Persona {
+    private String nombre;
+    private int edad;
+
+    public Persona(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void showInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+}
