1b551a6b3ef41ab7a06719e5589badb6630e0efc
danie <daniela.tobon@anahuac.mx>
2024-11-22 12:03:12 -0600

    Creacion del test

---
+++ src2/personaTest.java
@@ -0,0 +1,12 @@
+Class PersonaTest.java{
+    public static void main(String args[]){
+        persona rene=new persona("rene",45);
+        persona dani=new persona("dani",21);
+
+        rene.showInfo();
+        dani.showInfo();
+
+    }
+
+
+}
